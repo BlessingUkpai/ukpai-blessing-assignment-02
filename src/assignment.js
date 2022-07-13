493f54bd60e8ddefbe70192462642c1906ee3981
Blessing Ukpai <95421648+BlessingUkpai@users.noreply.github.com>
2022-07-13 09:36:20 +0000

    Check challenges 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -8,29 +8,77 @@ const assignment = {};
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
+
  * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
  */
+console.log('QUESTION 1 ANSWER')
+console.log('')
+
+let arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+let sum = 0;
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    
+for( let number = 0; number < arrayOfNumbers.length; number++ ){
+    sum = sum + arrayOfNumbers[number];
+   console.log(sum)
+
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+}
+
+sumOfNumbers(arrayOfNumbers)
+
+console.log('')
+
+console.log(`The number of elements in the array is ${arrayOfNumbers.length}`)
+
+
 
 /**
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
+
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
+ 
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+console.log('')
+console.log('')
+console.log('')
+console.log('')
+
+
+
+console.log('QUESTION 2 ANSWER')
+console.log('')
+let NewarrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+
+function countEvenNumbers(NewarrayOfNumbers) {
+
+// console.log(`The original array is [${arrayOfNumbers}] and it consists of ${arrayOfNumbers.length} elements`)
+console.log('The original array of numbers is');       
+console.log(arrayOfNumbers)
+
+console.log('')
+console.log('')
+
+
+let evenNumbers = arrayOfNumbers.filter(number => number % 2 == 0)
+console.log('The array of even numbers is');
+console.log(evenNumbers)
+
+console.log(`and there are ${evenNumbers.length} even numbers in this array`)
+
+
+    return NewarrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+countEvenNumbers(NewarrayOfNumbers)
+
+
+
 
 /**
  * Challenge - 3
